1512b610e7d4472724979225efe4260938952c33
Victoria Corona <corona.victoria@hotmail.com>
2020-05-28 10:19:47 -0700

    wordReformer function created

---
+++ solutions/word-reformer/main.js
@@ -0,0 +1,8 @@
+function wordReformer(wordFragment1, wordFragment2) {
+  return wordFragment1 + wordFragment2;
+}
+
+var wordReformerResults = wordReformer("bro", "ken");
+console.log(wordReformerResults);
+
+// add commit push origin word-reformer in master FOR NOW
